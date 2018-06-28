e761ab9a4aa30f25eb533d5edffaca4c7121cc10
redtobster <tobylimanto@gmail.com>
2018-06-28 14:56:14 +0800

    Update classifying_positions_v3.py

--- classifying_positions_v3.py
+++ classifying_positions_v3.py
@@ -21,7 +21,7 @@ def get_index_sex(df):
     df = df.fillna('empty')
     for row in list(df.index):
         for col in list(df.columns):
-            if df.loc[row, col] == 'FishSex:Male' or df.loc[row, col] == 'FishSex:Female':
+            if bool(re.match('FishSex.*', str(df.loc[row, col]))):
             	index_sex[0] = row
                 index_sex[1] = col
                 return index_sex
@@ -31,7 +31,7 @@ def get_index_fps(df):
     df = df.fillna('empty')
     for row in list(df.index):
         for col in list(df.columns):
-            if bool(re.match('FrameRate*', df.loc[row, col])):
+            if bool(re.match('FrameRate.*', str(df.loc[row, col]))):
                 index_fps[0] = row
                 index_fps[1] = col
                 return index_fps
