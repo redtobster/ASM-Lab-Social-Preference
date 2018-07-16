07b5257428a37363c309e2d174acf1a2368c9c14
redtobster <tobylimanto@gmail.com>
2018-07-16 20:09:39 +0800

    Update classifying_positions_v3.py
    Reduced number of functions needed.

--- analysis_paraphernalia_fun/classifying_positions_v3.py
+++ analysis_paraphernalia_fun/classifying_positions_v3.py
@@ -1,40 +1,7 @@
+from index_fun import get_index_x_y, get_index_sex, get_index_fps
 import pandas as pd
 import numpy as np
 import re
-
-# Functions to automatically get the index of 'x', 'y', 'fps' and 'FishSex' in a dataframe
-def get_index_x_y(df):
-    index_x = [0, 0]
-    index_y = [0, 0]
-    for row in list(df.index):
-        for col in list(df.columns):
-            if df.loc[row, col] == 'x':
-                index_x[0] = row
-                index_x[1] = col
-            if df.loc[row, col] == 'y':
-                index_y[0] = row
-                index_y[1] = col
-                return index_x, index_y
-
-def get_index_sex(df):
-    index_sex = [0, 0]
-    df = df.fillna('empty')
-    for row in list(df.index):
-        for col in list(df.columns):
-            if bool(re.match('FishSex.*', str(df.loc[row, col]))):
-            	index_sex[0] = row
-                index_sex[1] = col
-                return index_sex
-
-def get_index_fps(df):
-    index_fps = [0, 0]
-    df = df.fillna('empty')
-    for row in list(df.index):
-        for col in list(df.columns):
-            if bool(re.match('FrameRate.*', str(df.loc[row, col]))):
-                index_fps[0] = row
-                index_fps[1] = col
-                return index_fps
 
 # functions to assign x,y positions to their respective quadrants for the first two minutes
 # and the entire experiment
